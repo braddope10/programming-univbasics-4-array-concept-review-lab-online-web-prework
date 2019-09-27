f80b153a0d826daa55468697bba2dcb1359f22e0
Bradley Lopez <bradleylopezherrera@gmail.com>
2019-09-27 01:01:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,13 +9,17 @@
   # nil
 # end
 
+# def find_element_index(array, value_to_find)
+  # array.length.times do |counter|
+    # if array[counter] == value_to_find
+      # return counter 
+    # end 
+  # end 
+  # nil
+# end
+
 def find_element_index(array, value_to_find)
-  array.length.times do |counter|
-    if array[counter] == value_to_find
-      return counter 
-    end 
-  end 
-  nil
+  
 end
 
 def find_max_value(array)
